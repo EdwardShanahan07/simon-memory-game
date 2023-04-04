3d78b8070135e66b607109d4ecec19a1353a58c2
Edward Shanahan <edwardshanahan357@gmail.com>
2023-04-04 19:16:21 +0100

    Add tests for newGame function

--- scripts/game.js
+++ scripts/game.js
@@ -2,9 +2,19 @@ const game = {
   score: 0,
   currentGame: [],
   playerMoves: [],
-  choices: [],
+  choices: ["button1", "button2", "button3", "button4"],
 };
 
-console.log(game);
+const newGame = () => {
+  game.score = 0;
+  game.currentGame = [];
+  game.playerMoves = [];
 
-module.exports = { game };
+  showScore();
+};
+
+const showScore = () => {
+  document.getElementById("score").innerText = game.score;
+};
+
+module.exports = { game, newGame, showScore };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame } = require("../game");
 
 beforeAll(() => {
   const fs = require("fs");
@@ -28,5 +28,36 @@ describe("game object contains correct keys", () => {
 
   test("choices key exists", () => {
     expect("choices" in game).toBe(true);
+  });
+
+  test("choices contain correct ids", () => {
+    expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+  });
+});
+
+describe("new game works correctly", () => {
+  beforeAll(() => {
+    game.score = 42;
+    game.currentGame = ["button1", "button2"];
+    game.playerMoves = ["button1", "button2"];
+
+    document.getElementById("score").innerText = 42;
+    newGame();
+  });
+
+  test("should set score to zero", () => {
+    expect(game.score).toEqual(0);
+  });
+
+  test("current game array should be empty", () => {
+    expect(game.currentGame.length).toEqual(0);
+  });
+
+  test("player moves array should be empty", () => {
+    expect(game.playerMoves.length).toEqual(0);
+  });
+
+  test("should display zero with the element of id of score", () => {
+    expect(document.getElementById("score").innerText).toEqual(0);
   });
 });
